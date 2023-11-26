ef410240b98484842d26c9852b7e3b552386a227
hunglongtrangithub <hunglongtran2004@gmail.com>
2023-11-25 22:52:23 -0500

    add type validation to Note content

--- backend/models/note.js
+++ backend/models/note.js
@@ -18,7 +18,11 @@ mongoose
   });
 
 const noteSchema = new mongoose.Schema({
-  content: String,
+  content: {
+    type: String,
+    minLength: 5,
+    required: true,
+  },
   important: Boolean,
 });
 
