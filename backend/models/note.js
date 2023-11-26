98abaef555a58f9f5d8ba6501cf4a6731952782c
hunglongtrangithub <hunglongtran2004@gmail.com>
2023-11-26 16:10:08 -0500

    add code changes for part4a

--- backend/models/note.js
+++ backend/models/note.js
@@ -1,37 +1,20 @@
-const mongoose = require('mongoose')
-require('dotenv').config()
-
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose
-  .connect(url)
-
-  .then((result) => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+const mongoose = require("mongoose");
 
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minLength: 5,
     required: true,
+    minlength: 5,
   },
   important: Boolean,
-})
+});
 
-noteSchema.set('toJSON', {
+noteSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
   },
-})
+});
 
-module.exports = mongoose.model('Note', noteSchema)
+module.exports = mongoose.model("Note", noteSchema);
