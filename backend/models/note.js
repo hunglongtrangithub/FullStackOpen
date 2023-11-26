0b4d19ff9de8d9ce88bfd3ae1dbf35710097d3c1
hunglongtrangithub <hunglongtran2004@gmail.com>
2023-11-26 00:45:11 -0500

    part3 solutions

--- backend/models/note.js
+++ backend/models/note.js
@@ -1,21 +1,21 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log("connecting to", url);
+console.log('connecting to', url)
 
 mongoose
   .connect(url)
 
   .then((result) => {
-    console.log("connected to MongoDB");
+    console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const noteSchema = new mongoose.Schema({
   content: {
@@ -24,14 +24,14 @@ const noteSchema = new mongoose.Schema({
     required: true,
   },
   important: Boolean,
-});
+})
 
-noteSchema.set("toJSON", {
+noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Note", noteSchema);
+module.exports = mongoose.model('Note', noteSchema)
--- backend/mongo.js
+++ backend/mongo.js
@@ -1,23 +1,23 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
+  console.log('give password as argument')
+  process.exit(1)
 }
 
-const password = process.argv[2];
-const databaseName = "noteApp";
-const url = `mongodb+srv://hunglongtran2004:${password}@cluster0.pme8x5t.mongodb.net/${databaseName}?retryWrites=true&w=majority`;
+const password = process.argv[2]
+const databaseName = 'noteApp'
+const url = `mongodb+srv://hunglongtran2004:${password}@cluster0.pme8x5t.mongodb.net/${databaseName}?retryWrites=true&w=majority`
 
-mongoose.set("strictQuery", false);
-mongoose.connect(url);
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
-});
+})
 
-const Note = mongoose.model("Note", noteSchema);
+const Note = mongoose.model('Note', noteSchema)
 // Note.find({ important: { $ne: true } }).then((result) => {
 //   result.forEach((note) => {
 //     console.log(note);
@@ -26,14 +26,14 @@ const Note = mongoose.model("Note", noteSchema);
 // });
 
 let notes = [
-  { id: 1, content: "HTML is easy", important: true },
-  { id: 2, content: "Browser can execute only JavaScript", important: false },
+  { id: 1, content: 'HTML is easy', important: true },
+  { id: 2, content: 'Browser can execute only JavaScript', important: false },
   {
     id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
+    content: 'GET and POST are the most important methods of HTTP protocol',
     important: true,
   },
-];
+]
 
 Note.insertMany(
   notes.map(
@@ -44,6 +44,6 @@ Note.insertMany(
       })
   )
 ).then((result) => {
-  console.log(result);
-  mongoose.connection.close();
-});
+  console.log(result)
+  mongoose.connection.close()
+})
