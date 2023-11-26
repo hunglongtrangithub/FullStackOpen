253387b3958efaa4b9dd6d893ae04c8a97359553
hunglongtrangithub <hunglongtran2004@gmail.com>
2023-11-26 16:15:00 -0500

    change folder name: phonebook -> phonebook-frontend

--- part2/phonebook/src/components/Filter.jsx
+++
@@ -1,9 +0,0 @@
-const Filter = ({ value, onChange }) => {
-  return (
-    <div>
-      filter shown with<input value={value} onChange={onChange}></input>
-    </div>
-  );
-};
-
-export default Filter;
--- part2/phonebook/src/components/Notification.jsx
+++
@@ -1,8 +0,0 @@
-const Notification = ({ message, type }) => {
-  if (message === null) {
-    return null;
-  }
-  return <div className={type}>{message}</div>;
-};
-
-export default Notification;
