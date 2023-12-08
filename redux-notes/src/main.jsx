8e93d08d55449f2f5c42780bc6bb4fe3c31e2dc9
hunglongtrangithub <hunglongtran2004@gmail.com>
2023-12-07 20:23:56 -0500

    notes app using redux (from part6a)

---
+++ redux-notes/src/main.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import App from "./App";
+import noteReducer from "./reducers/noteReducer";
+
+const store = createStore(noteReducer);
+
+store.dispatch({
+  type: "NEW_NOTE",
+  data: {
+    content: "the app state is in redux store",
+    important: true,
+    id: 1,
+  },
+});
+
+store.dispatch({
+  type: "NEW_NOTE",
+  data: {
+    content: "state changes are made with actions",
+    important: false,
+    id: 2,
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
