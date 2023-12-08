880f72be31e4faec78385f2aecb9a1a4f5339bdf
hunglongtrangithub <hunglongtran2004@gmail.com>
2023-12-07 20:27:11 -0500

    move backend and frontend to notes-app

--- backend/index.js
+++
@@ -1,8 +0,0 @@
-const app = require("./app");
-const config = require("./utils/config");
-const logger = require("./utils/logger");
-
-app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`);
-  logger.info(`localhost:${config.PORT}`);
-});
