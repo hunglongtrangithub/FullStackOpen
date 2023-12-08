2cf83b0ed2af3087577a55eb502336d29d439cfb
hunglongtrangithub <hunglongtran2004@gmail.com>
2023-12-07 21:52:19 -0500

    use combineReducers (part6b)

--- redux-notes/src/App.jsx
+++ redux-notes/src/App.jsx
@@ -1,10 +1,16 @@
 import NewNote from "./components/NewNote";
 import Notes from "./components/Notes";
+import VisibilityFilter from "./components/VisibilityFilter";
 
 const App = () => {
+  const filterSelected = (value) => {
+    console.log(value);
+  };
+
   return (
     <div>
       <NewNote />
+      <VisibilityFilter />
       <Notes />
     </div>
   );
--- redux-notes/src/main.jsx
+++ redux-notes/src/main.jsx
@@ -1,30 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
 
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
 import App from "./App";
+
 import noteReducer from "./reducers/noteReducer";
 
-const store = createStore(noteReducer);
+import filterReducer from "./reducers/filterReducer";
 
-store.dispatch({
-  type: "NEW_NOTE",
-  data: {
-    content: "the app state is in redux store",
-    important: true,
-    id: 1,
-  },
+import { createNote } from "./reducers/noteReducer";
+import { filterChange } from "./reducers/filterReducer";
+
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer,
 });
 
-store.dispatch({
-  type: "NEW_NOTE",
-  data: {
-    content: "state changes are made with actions",
-    important: false,
-    id: 2,
-  },
-});
+const store = createStore(reducer);
+
+console.log(store.getState());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
