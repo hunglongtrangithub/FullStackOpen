66b6c6574c52939ab7cf70e0a68de29494c6adac
hunglongtrangithub <hunglongtran2004@gmail.com>
2023-12-08 20:11:52 -0500

    notes app with react query (part6d)

---
+++ query-notes/src/App.jsx
@@ -0,0 +1,65 @@
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getNotes, createNote, updateNote } from "./requests";
+
+const App = () => {
+  const queryClient = useQueryClient();
+
+  const newNoteMutation = useMutation({
+    mutationFn: createNote,
+    onSuccess: (newNote) => {
+      const notes = queryClient.getQueryData(["notes"]);
+      queryClient.setQueryData(["notes"], notes.concat(newNote));
+    },
+  });
+
+  const addNote = async (event) => {
+    event.preventDefault();
+    const content = event.target.note.value;
+    event.target.note.value = "";
+    newNoteMutation.mutate({ content, important: true });
+  };
+
+  const updateNoteMutation = useMutation({
+    mutationFn: updateNote,
+    onSuccess: (updatedNote) => {
+      const notes = queryClient.getQueryData(["notes"]);
+      queryClient.setQueryData(
+        ["notes"],
+        notes.map((note) => (note.id === updatedNote.id ? updatedNote : note))
+      );
+    },
+  });
+
+  const toggleImportance = (note) => {
+    updateNoteMutation.mutate({ ...note, important: !note.important });
+  };
+
+  const result = useQuery({
+    queryKey: ["notes"],
+    queryFn: getNotes,
+  });
+  console.log(JSON.parse(JSON.stringify(result)));
+  if (result.isLoading) {
+    return <div>loading data...</div>;
+  }
+
+  const notes = result.data;
+
+  return (
+    <div>
+      <h2>Notes app</h2>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
+      {notes.map((note) => (
+        <li key={note.id} onClick={() => toggleImportance(note)}>
+          {note.content}
+          <strong> {note.important ? "important" : ""}</strong>
+        </li>
+      ))}
+    </div>
+  );
+};
+
+export default App;
